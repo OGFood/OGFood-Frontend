82f2728c4fee04c725fa3db3212cbecce7fa92a3
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-05-06 13:36:55 +0200

    Cleaned up project, added folder structure

---
+++ GoodFood/src/style/muitheme.jsx
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material/styles"
+
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#eedbcd"
+		},
+		secondary: {
+			main: "#325044"
+		},
+	},
+})
+
+export default theme;
