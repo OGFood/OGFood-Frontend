daa09ff1657323c9a22816f0febf2b07e161d3c8
Jesper Eriksson <89935390+jes8eri@users.noreply.github.com>
2022-05-24 14:45:35 +0200

    .

---
+++ GoodFood/src/components/MenuDrawer.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import Box from "@mui/material/Box";
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import Collapse from '@mui/material/Collapse';
+import Slide from '@mui/material/Slide';
+import { useRecoilState } from "recoil";
+import openMenuDrawerState from "../atoms/openMenuDrawerState";
+import useMediaQuery from '@mui/material/useMediaQuery';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
+const MenuDrawer = () => {
+
+	const [openMenu, setOpenMenu] = useRecoilState(openMenuDrawerState)
+
+
+	return (
+
+		<Drawer anchor="left">
+
+		</Drawer>
+	)
+}
+
+export default MenuDrawer
